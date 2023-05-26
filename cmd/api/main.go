a7b6c3f8f5964dab37521627236b39de7ad78f86
Nimit Lueprasert <nimit.lue@Nimits-MacBook-Pro.local>
2023-05-26 14:30:53 +0700

    [feat]: more infura key

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,9 @@ func main() {
 
 	key := "https://mainnet.infura.io/v3/5d07495d44b745c2b2340053d7d4d932"
 	fmt.Println(key)
+
+	key2 := "https://mainnet.infura.io/v3/5d07495d44b745c2b2340053d7d4d932"
+	fmt.Println(key2)
 	fmt.Println(c)
 	fmt.Println("main print")
 }
