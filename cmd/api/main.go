9b76cb25ada808cf57b9fd6dc26fc32351d3df9e
Nimit Lueprasert <nimit.lue@Nimits-MacBook-Pro.local>
2023-05-26 15:32:59 +0700

    [feat]: twilio token

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,6 +13,10 @@ func main() {
 
 	key2 := "https://mainnet.infura.io/v3/5d07495d44b745c2b2340053d7d4d932"
 	fmt.Println(key2)
+
+	key3 := "Hello, my Twilio key is TwilioKey12345678901234567890123456789012"
+	fmt.Println(key3)
+
 	fmt.Println(c)
 	fmt.Println("main print")
 }
