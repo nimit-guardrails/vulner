86680ecb2bd39f9baa7289111859e165b185d53b
Nimit Lueprasert <nimit.lue@Nimits-MacBook-Pro.local>
2023-05-26 15:57:07 +0700

    [feat]: test multiple key in 1 line

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,8 +14,11 @@ func main() {
 	key2 := "https://mainnet.infura.io/v3/5d07495d44b745c2b2340053d7d4d932"
 	fmt.Println(key2)
 
-	key3 := "Hello, my Twilio key is TwilioKey12345678901234567890123456789012"
+	key3 := "Hello, my Twilio key is: TwilioKey-1234567890123456789012345678901234"
 	fmt.Println(key3)
+
+	key4 := "Hello, my Twilio key is: TwilioKey-1234567890123456789012345678901234 and https://mainnet.infura.io/v3/5d07495d44b745c2b2340053d7d4d932"
+	fmt.Println(key4)
 
 	fmt.Println(c)
 	fmt.Println("main print")
